5497bd40c65d266622aa6263cb4144ed670ef0eb
root <richkibz@gmail.com>
2023-10-28 07:18:53 +0300

    refractor

--- Domestic/Domestic/settings.py
+++ Domestic/Domestic/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c9$l1(xvi9r8gx3ny)&1&(ytcr$25^$1041&nb=b5s!=wo6c6e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
